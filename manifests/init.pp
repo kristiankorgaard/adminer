aaf6da766288ef21388c7ebeee157d24e9c92d08
Kristian Gram Krogh <kristian@korgaard.com>
2018-10-28 19:15:47 +0100

    Adding template for Apache configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,10 @@
 class adminer (
-    String              $adminer_url        = 'https://www.adminer.org/latest.php',
-    String              $install_dir        = $::adminer::params::install_dir,
-    String              $apache_conf_dir    = $::adminer::params::apache_config_dir,
-    Array[ String ]     $allow_list         = [ '127.0.0.1' ],
+    String              $adminer_url                = 'https://www.adminer.org/latest.php',
+    String              $install_dir                = $::adminer::params::install_dir,
+    String              $apache_default_config      = $::adminer::params::apache_default_config,
+    String              $apache_name                = $::adminer::params::apache_name,
+    Array[ String ]     $allow_list                 = [ '127.0.0.1' ],
+    String              $apache_alias               = 'adminer',
     ) inherits ::adminer::params {
     
     include wget
@@ -17,6 +19,18 @@ class adminer (
         timeout         => 0,
         verbose         => false,
         require         => File[ $install_dir ],
+    }
+    
+    file { $apache_default_config:
+        ensure          => present,
+        content         => epp('adminer/adminer.conf.epp', {
+                                'apache_alias' => $apache_alias,
+                                'install_dir'  => $install_dir,
+                                'allow_list'   => $allow_list,
+                            }
+                        ),
+        require         => File[ $install_dir ],
+        notify          => Service[ $apache_name ],
     }
     
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,13 @@
 class adminer::params {
     include ::apache::params
     
+    $apache_name  = $::apache::params::apache_name
+    
     case $::osfamily {
         'RedHat': {
             $install_dir            = '/usr/share/adminer'            
             $apache_config_dir      = $::apache::params::confd_dir
+            $apache_default_config  = "${::apache::params::confd_dir}/00-adminer.conf"
         }
         default: {
             fail("Class['adminer::params']: Unsupported OS: ${::osfamily}")
